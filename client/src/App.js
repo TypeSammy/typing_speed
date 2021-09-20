eda15dcea267fb172c5fbc1988d2fd449d58cec3
Samantha <typesammy@gmail.com>
2021-09-20 20:50:51 +1000

    commit before heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import { useState, useEffect } from 'react'
-import Leaderboard from './component/Leaderboard'
+import { useState } from 'react'
 import TypingSpeed from './component/TypingSpeed'
-import axios from 'axios'
 
 function App() {
-  const [leaderboard, setLeaderboard] = useState([])
-
-  useEffect(() => {
-    axios.get('/api/leaderboard')
-      .then(res => setLeaderboard(res.data))
-  }, [])
-
   return (
     <div className="App">
       <TypingSpeed />
-      <Leaderboard leaderData={leaderboard} />
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ const PORT = 3001
 app.use(express.json())
 
 const paragraphsController = require('./controllers/paragraphs_controller.js')
-const leaderboardController = require('./controllers/leaderboard_conroller')
 
 app.use('/api/paragraphs', paragraphsController)
-app.use('/api/leaderboard', leaderboardController)
 
 app.listen(PORT, () => {
   console.log(`*** Listening on port ${PORT} ***`)
 })
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
